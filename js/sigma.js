e485003314b2492582e58907d618387555460e32
shima15 <s15187so@sfc.keio.ac.jp>
2016-05-26 10:30:13 +0900

    関数squareSumの定義

--- js/sigma.js
+++ js/sigma.js
@@ -1,1 +1,12 @@
 const N10 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function squareSum(n){
+    var sum = 0;
+    var i = 1;
+    
+    while(i <= n){
+        sum = sum + i; //i = 1;
+        i = i + 1;
+    }
+    return sum;
+}

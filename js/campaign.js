4d022333a642340cfa09221bd3e9feb15edb9904
shima15 <s15187so@sfc.keio.ac.jp>
2016-06-09 10:58:03 +0900

    03

---
+++ js/campaign.js
@@ -0,0 +1,16 @@
+function campaignNumber(user){
+    if(user.age >= 30 &&
+       user.sex == 1 &&
+       user.married == true){
+           return 1;
+           
+       }
+ 
+ 
+    var user = {
+        age: age,
+        sex: sex,
+        married: married,
+        campaignNumber: campaignNumber
+    }
+        return user;
